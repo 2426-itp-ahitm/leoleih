e95bb089bd336a83203f7c721ead6c3c9a3e233f
UuBroot <lolylyt@gmail.com>
2024-11-11 15:17:54 +0100

    finished design 1

--- Designs/1Centered/main.js
+++ Designs/1Centered/main.js
@@ -2,8 +2,9 @@
  * Document imports
  */
 const results = document.getElementById("results")
-const cathegory = document.querySelector('.cathegory');
-const categoryIcon = document.querySelector('.categoryIcon')
+const cathegorys = document.getElementById("cathegorys")
+const cathegory = document.querySelectorAll('.cathegory');
+const categoryIcon = document.querySelectorAll('.categoryIcon')
 
 /**
  * General Variables
@@ -12,7 +13,6 @@ let currentNavBarStatus = 0 //0=expandet 1=small
 
 
 //NavBar Expantion
-setInterval(checkIfResultsExist, 1000);
 function checkIfResultsExist(){
     if(results.innerHTML.trim().length === 0){
         currentNavBarStatus = 0
@@ -25,9 +25,35 @@ function checkIfResultsExist(){
 function updateNavBarStatus(){
     if (currentNavBarStatus === 1){
         console.log("small");
-        categoryIcon.style.transform = "scale(3)"
-        cathegory.style.width = "70px"
-        cathegory.style.height = "40px"
+        categoryIcon.forEach(element => {
+            element.style.transform = "scale(0.8)"
+        })
+        cathegory.forEach(element => {
+            element.style.width = "70px"
+            element.style.height = "40px"
+        })
+        cathegorys.style.gap = "2px"
+        cathegorys.style.gridTemplateColumns = "repeat(4, 80px)"
+    }else{
 
+        console.log("small");
+        categoryIcon.forEach(element => {
+            element.style.transform = "scale(3)"
+        })
+        cathegory.forEach(element => {
+            element.style.width = "200px"
+            element.style.height = "200px"
+        })
+        cathegorys.style.gap = "30px"
+        cathegorys.style.gridTemplateColumns = "repeat(4, 200px)"
     }
+}
+
+function generateEntries(){
+    let string = ""
+    for(let i = 0;i<10;i++){
+        string+="<div class='resultBox'>data</div>"
+    }
+    results.innerHTML = string
+    checkIfResultsExist()
 }
