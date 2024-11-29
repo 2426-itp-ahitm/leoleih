8f0b16b34ce2f11d216427c95f47c18d5b525099
UuBroot <lolylyt@gmail.com>
2024-11-29 12:57:54 +0100

    added searchbar

--- Application/Frontend/src/components/searchbar.ts
+++ Application/Frontend/src/components/searchbar.ts
@@ -3,16 +3,15 @@ import { style } from "./css_searchbar"
 
 const content = html`
     ${style}
-    <script>
-console.log("dsdf")
-    </script>
-    <input onchange="generateEntries()" type="text" id="searchbar" placeholder="Search">
+    <input type="text" id="searchbar" placeholder="Search">
 `
 
 class Module extends HTMLElement {
     constructor(){
         super()
         this.attachShadow({mode: "open"})
+
+        
     }
     connectedCallback() {
         render(content, this.shadowRoot)
