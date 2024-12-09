8151e24b4d326aec517820bc279795a5932e5332
UuBroot <81309724+UuBroot@users.noreply.github.com>
2024-12-09 13:24:49 +0100

    refactored render #74

--- Application/Backend/src/main/java/at/htl/leonding/leih/model/FotoCam.java
+++ Application/Backend/src/main/java/at/htl/leonding/leih/model/FotoCam.java
@@ -6,17 +6,18 @@ import java.util.ArrayList;
 import java.util.List;
 //@NamedQuery(name=FotoCam.QUERY_FIND_ALL, query="SELECT i FROM Item i where i.item_description = 'photo_camera'")
 
-public class FotoCam extends Item{
-    public FotoCam(){
+public class FotoCam extends Item {
+    public FotoCam() {
         super();
     }
+
     public static List<FotoCam> fotosCams = new ArrayList<>();
 
-    public static List<FotoCam> getallFotosCams(){
+    public static List<FotoCam> getallFotosCams() {
         return fotosCams;
     }
 
-    public void addFotoCam(FotoCam fotoCam){
+    public void addFotoCam(FotoCam fotoCam) {
         fotosCams.add(fotoCam);
     }
 
