950ca08f717c0fc777dde37b518ce94ecfda6d1d
UuBroot <81309724+UuBroot@users.noreply.github.com>
2025-01-12 17:21:00 +0100

    fixed server adress

--- Application/Frontend/src/model/item-service.ts
+++ Application/Frontend/src/model/item-service.ts
@@ -1,7 +1,7 @@
 import { Item } from "./item";
 
 export async function loadItems(){
-    const response = await fetch("http://localhost:8080/getAll/fotoCams")
+    const response = await fetch("http://localhost:8080/items")
     const items = await response.json() as Item[]
     return items
 }
