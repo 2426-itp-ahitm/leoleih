e46bde13ea7d8f62e86e924b731844ab21b66bcb
UuBroot <81309724+UuBroot@users.noreply.github.com>
2025-01-13 11:20:58 +0100

    made detailview css better

--- Application/Frontend/src/main/detailview/css_detailview.ts
+++ Application/Frontend/src/main/detailview/css_detailview.ts
@@ -8,6 +8,13 @@ const style = html`
             height:60%;
             top: 50%;
             bottom: 50%;
+            border-radius: 30px;
+            justify-content: center;
+            align-items: center;
+
+            border: 3px solid rgba(41, 47, 57, 0.70);
+            background-color:rgba(41, 47, 57, 0.76);
+            backdrop-filter: blur(8px);
         }
     </style>
 `
