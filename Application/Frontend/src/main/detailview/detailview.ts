53c964c92729e9541c7e90e5cdca023257729cda
UuBroot <81309724+UuBroot@users.noreply.github.com>
2025-01-13 12:24:01 +0100

    removed click listener in detailview

--- Application/Frontend/src/main/detailview/detailview.ts
+++ Application/Frontend/src/main/detailview/detailview.ts
@@ -13,10 +13,6 @@ class Module extends HTMLElement {
     constructor(){
         super()
         this.attachShadow({mode: "open"})
-
-        this.shadowRoot.addEventListener('click', (event) => {
-            this.shadowRoot.querySelector("dialog").close()
-        });
     }
     async content(){
         const item = await loadItem(this.detailsForId)
@@ -29,7 +25,7 @@ class Module extends HTMLElement {
                     <p>category:${item.item_category}</p>
                     <p>set:${item.item_set}</p>
                     <p>type:${item.item_type}</p>
-                    <button>Close</button>
+                    <button @click=${()=>this.shadowRoot.querySelector("dialog").close()}>Close</button>
                 </dialog>
             `
         }catch(e){
