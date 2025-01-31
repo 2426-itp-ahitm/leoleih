ec3a5091b449d0698a3304dd4dc5afd42cc04d7d
UuBroot <81309724+UuBroot@users.noreply.github.com>
2025-01-31 13:23:42 +0100

    fixed detail dialog not closing

--- Application/Frontend/src/main/detailview/detailview.ts
+++ Application/Frontend/src/main/detailview/detailview.ts
@@ -1,72 +1,66 @@
-import { html, render } from "lit-html"
-import { style } from "./css_detailview"
-import {global} from "../../global"
-import {loadItem} from "../../model/item-service"
-const HTML_NAME = "custom-detailview"//must contain - because webpack
+import { html, render } from "lit-html";
+import { style } from "./css_detailview";
+import { global } from "../../global";
+import { loadItem } from "../../model/item-service";
+const HTML_NAME = "custom-detailview"; //must contain - because webpack
 
 class Module extends HTMLElement {
-    detailsForId: number = 0;
-    open: boolean = true
-    static get observedAttributes() {
-        return ['id','open'];
-    }
-    constructor(){
-        super()
-        this.attachShadow({mode: "open"})
-    }
-    async content(){
-        const item = await loadItem(this.detailsForId)
-        
-        try{            
-            return html`
-            ${style}
-                <dialog id="detailDialog${this.id}">
-                    <p>description:${item.item_description}</p>
-                    <p>category:${item.item_category}</p>
-                    <p>set:${item.item_set}</p>
-                    <p>type:${item.item_type}</p>
-                    <button @click=${()=>this.shadowRoot.querySelector("dialog").close()}>Close</button>
-                </dialog>
-            `
-        }catch(e){
-            //console.log(e);
-        }
+  detailsForId = 0;
+  open = true;
+  static get observedAttributes() {
+    return ["id", "open"];
+  }
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+  async content() {
+    const item = await loadItem(this.detailsForId);
 
-    }
-    getOpenStatus(){
-        console.log(this.open);
-        
-        if (this.open){
-            return open
-        }else{
-            return
-        }
-    }
-    attributeChangedCallback(name, _oldValue, newValue) {
-        switch(name){
-            case "id":                              
-                this.detailsForId = parseInt(newValue)
-                break;
-            case "open":
-                console.log(newValue);
-                
-                if (newValue === "true"){
-                    try{
-                        console.log(this.shadowRoot.children);
-                        
-                        this.shadowRoot.querySelector("dialog").show()
+    return html`
+      ${style}
+      <dialog id="detailDialog${this.id}">
+        <p>description:${item.item_description}</p>
+        <p>category:${item.item_category}</p>
+        <p>set:${item.item_set}</p>
+        <p>type:${item.item_type}</p>
+        <button @click=${() => this.closeDialog()}>
+          Close
+        </button>
+      </dialog>
+    `;
+  }
+  closeDialog(){
+    const dialog = this.shadowRoot.querySelector("dialog");
+    console.log('dialog:',dialog);
+    dialog.close();
+    setTimeout(() => {//"Fixes" a bug where the dialog would open again after closing
+      this.setAttribute("open", "false");
+      dialog.close();
+    }, 10);
+  }
+  attributeChangedCallback(name: "id" | "open", _: string, newValue: string) {
+    switch (name) {
+      case "id":
+        this.detailsForId = parseInt(newValue);
+        break;
+      case "open":
+        console.log('att change open:',newValue);
 
-                    }catch(e){
-                        console.log(e);
-                    }
-                }
-                break;
+        if (newValue === "true") {
+          try {
+            this.shadowRoot.querySelector("dialog").show();
+          } catch (e) {
+            console.log(e);
+          }
         }
-        
-        this.connectedCallback()
+        break;
     }
-    async connectedCallback() {
-        render( await this.content(), this.shadowRoot)
-    }
+
+    this.connectedCallback();
+  }
+  async connectedCallback() {
+    render(await this.content(), this.shadowRoot);
+  }
 }
-customElements.define(HTML_NAME, Module)
+customElements.define(HTML_NAME, Module);
