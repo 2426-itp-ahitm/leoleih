656cb312c1c5e585fab65a314339adbd15fa9dfc
UuBroot <81309724+UuBroot@users.noreply.github.com>
2025-03-28 12:50:30 +0100

    updated css and other things

--- Application/Frontend/src/components/main/item/css_item.ts
+++ Application/Frontend/src/components/main/item/css_item.ts
@@ -1,30 +1,30 @@
-import { html } from "lit-html"
+import { html } from "lit-html";
 const style = html`
-    <style>
-        div{
-            width: 200px;
-            height: 150px;
-            background-color: rgb(26, 38, 49);
-            border-radius: 10px;
-            border: 3px none rgba(42, 57, 65, 0.405);
-            box-shadow: 0px 4px 40px 5px rgba(42, 57, 65, 0.405);
-            display: grid;
-            align-items: center;
-            justify-content: center;
-            transition: all 0.3s ease-out;
-            cursor: pointer;
-        }
-        div:hover{
-            box-shadow: 0 4px 40px 5px rgba(71, 96, 110, 0.6);
-        }
-        svg{
-            margin-right: 10px;
-        }
-        #box{
-            width: 10vw;
-            height: 9vh;
-            padding: 30px;
-        }
-    </style>
-`
-export {style}
+  <style>
+    div {
+      width: 200px;
+      height: 150px;
+      background-color: rgb(26, 38, 49);
+      border-radius: 10px;
+      border: 3px none rgba(42, 57, 65, 0.405);
+      box-shadow: 0px 4px 40px 5px rgba(42, 57, 65, 0.405);
+      display: grid;
+      align-items: center;
+      justify-content: center;
+      transition: all 0.3s ease-out;
+      cursor: pointer;
+    }
+    div:hover {
+      box-shadow: 0 4px 40px 5px rgba(71, 96, 110, 0.6);
+    }
+    svg {
+      margin-right: 10px;
+    }
+    #box {
+      width: 10vw;
+      height: 9vh;
+      padding: 30px;
+    }
+  </style>
+`;
+export { style };
--- Application/Frontend/src/components/main/main.ts
+++ Application/Frontend/src/components/main/main.ts
@@ -1,58 +1,54 @@
-import {html, render} from "lit-html";
-import {style} from "./css_main";
-import {store} from "../../store";
+import { html, render } from "lit-html";
+import { style } from "./css_main";
+import { store } from "../../store";
 import "./detailview/detailview";
 import "./item/itemelem";
-import {distinctUntilChanged, map} from "rxjs";
-import {Item} from "Model/item";
+import { distinctUntilChanged, map } from "rxjs";
+import { Item } from "Model/item";
 
 const HTML_NAME = "custom-main";
 
 function itemTemplate(item) {
-    if (item == undefined) {
-        return html``;
-    }
-    return html`
-        <custom-item dev_id="${item.dev_id}" dev_type="${item.dev_type}" dev_category="${item.dev_category}"
-                     dev_serial_nr="${item.dev_serial_nr}" dev_asset_nr="${item.dev_asset_nr}"
-                     lent_from="${item.lent_from}" return_date="${item.return_date}" notes="${item.notes}"
-                     dev_set="${item.dev_set}"></custom-item>
-    `
+  if (item == undefined) {
+    return html``;
+  }
+  return html`
+        <custom-item dev_id="${item.dev_id}"}"></custom-item>
+    `;
 }
 
 class Module extends HTMLElement {
-    constructor() {
-        super()
-        this.attachShadow({mode: "open"});
-    }
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
 
-    content(items: Item[]) {
-        const elements = [];
-        for (let i = 0; i < items.length; i++) {
-            elements.push(itemTemplate(items[i]));
-        }
-        return html`
-            ${style}
-            <div style="opacity: ${elements.length === 0 ? 0 : 1}">
-                ${elements}
-            </div>
-        `;//TODO:custom-detailview
+  content(items: Item[]) {
+    const elements = [];
+    for (let i = 0; i < items.length; i++) {
+      elements.push(itemTemplate(items[i]));
     }
+    return html`
+      ${style}
+      <div style="opacity: ${elements.length === 0 ? 0 : 1}">${elements}</div>
+      <custom-detailview></custom-detailview>
+    `;
+  }
 
-    connectedCallback() {
-        store
-            .pipe(
-                map(module=>module.items),
-                distinctUntilChanged()
-            )
-            .subscribe(items => {
-            this.renderHTML(items);
-        })
-    }
+  connectedCallback() {
+    store
+      .pipe(
+        map((module) => module.items),
+        distinctUntilChanged(),
+      )
+      .subscribe((items) => {
+        this.renderHTML(items);
+      });
+  }
 
-    async renderHTML(items: Item[]) {
-        render(this.content(items), this.shadowRoot);
-    }
+  async renderHTML(items: Item[]) {
+    render(this.content(items), this.shadowRoot);
+  }
 }
 
 customElements.define(HTML_NAME, Module);
