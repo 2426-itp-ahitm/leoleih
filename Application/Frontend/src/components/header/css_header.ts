a7b566d8118727418ee4413eedeb1a5510dc75b6
SophiaLehner <s.lehner1@students.htl-leonding.ac.at>
2025-03-31 13:03:13 +0200

    new css

--- Application/Frontend/src/components/header/css_header.ts
+++ Application/Frontend/src/components/header/css_header.ts
@@ -3,7 +3,6 @@ import { html } from "lit-html";
 const style = html`
     <style>
         svg {
-            user-select: none;
             width: 300px;
             margin-bottom: 2em;
         }
@@ -12,8 +11,6 @@ const style = html`
             display: grid;
             text-align: center;
             margin-top: 20vh;
-            font-family: "Passion One", sans-serif;
-            position: sticky;
         }
 
         a {
