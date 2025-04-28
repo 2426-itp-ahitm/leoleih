451ad65e4d3bac6bbc79bab4c9cc5e90b62edc03
UuBroot <81309724+UuBroot@users.noreply.github.com>
2025-04-28 09:24:46 +0200

    added cart

--- Application/Frontend/src/components/main/main.ts
+++ Application/Frontend/src/components/main/main.ts
@@ -23,7 +23,7 @@ class Module extends HTMLElement {
     this.attachShadow({ mode: "open" });
   }
 
-  content(items: Item[], detailItem: Item | undefined) {
+  content(items: Item[], detailItem: Item | undefined, cartOpen: boolean) {
     const elements = [];
     for (let i = 0; i < items.length; i++) {
       elements.push(itemTemplate(items[i]));
@@ -32,36 +32,48 @@ class Module extends HTMLElement {
     return html`
       ${style}
       <div style="opacity: ${elements.length === 0 ? 0 : 1}">${elements}</div>
-      <div id="basket"></div>
+
+      <button id="cartButton" @click=${() => this.openCartDialog()}></button>
+
       ${detailItem != undefined
         ? html`
             <dialog id="detailDialog" open>
               <h2>${detailItem.dev_type}</h2>
-              <p>Notes: ${detailItem.notes}</p>
+              ${detailItem.notes != ""
+                ? html`<p>Notizen: ${detailItem.notes}</p>`
+                : html`Keine Notizen`}
               <button @click=${() => this.closeDetailDialog()}>Close</button>
             </dialog>
           `
         : html``}
-      ${store.getValue().basketOpen
+      ${cartOpen
         ? html`
-            <dialog id="basketDialog" open>
-              <h2>Basket</h2>
-              <p>Items in basket</p>
-              <button @click=${() => this.closeBasketDialog()}>Close</button>
+            <dialog id="cartDialog" open>
+              <h2>cart</h2>
+              <p>Items in card</p>
+              <button @click=${() => this.closeCartDialog()}>Close</button>
             </dialog>
           `
         : html``}
     `;
   }
+  //Detail Dialog
   closeDetailDialog() {
     const newState = produce(store.getValue(), (draft) => {
       draft.detailItem = undefined;
     });
     store.next(newState);
   }
-  closeBasketDialog() {
+  //Cart Dialog
+  openCartDialog() {
     const newState = produce(store.getValue(), (draft) => {
-      draft.basketOpen = false;
+      draft.cartOpen = true;
+    });
+    store.next(newState);
+  }
+  closeCartDialog() {
+    const newState = produce(store.getValue(), (draft) => {
+      draft.cartOpen = false;
     });
     store.next(newState);
   }
@@ -71,19 +83,26 @@ class Module extends HTMLElement {
         map((module) => ({
           items: module.items,
           detailItem: module.detailItem,
+          cartOpen: module.cartOpen,
         })),
         distinctUntilChanged(
           (prev, curr) =>
-            prev.items === curr.items && prev.detailItem === curr.detailItem,
+            prev.items === curr.items &&
+            prev.detailItem === curr.detailItem &&
+            prev.cartOpen === curr.cartOpen,
         ),
       )
-      .subscribe(({ items, detailItem }) => {
-        this.renderHTML(items, detailItem);
+      .subscribe(({ items, detailItem, cartOpen }) => {
+        this.renderHTML(items, detailItem, cartOpen);
       });
   }
 
-  async renderHTML(items: Item[], detailItem: Item | undefined) {
-    render(this.content(items, detailItem), this.shadowRoot);
+  async renderHTML(
+    items: Item[],
+    detailItem: Item | undefined,
+    basketOpen: boolean,
+  ) {
+    render(this.content(items, detailItem, basketOpen), this.shadowRoot);
   }
 }
 
