ff933d1652daceb12e86e2e9511251af8fb9ccd2
UuBroot <81309724+UuBroot@users.noreply.github.com>
2025-04-28 09:24:34 +0200

    redid main.css for better dialogs

--- Application/Frontend/src/components/main/css_main.ts
+++ Application/Frontend/src/components/main/css_main.ts
@@ -12,10 +12,22 @@ const style = html`
     }
     dialog {
       position: fixed;
-      top: 50%;
-      left: 50%;
+      top: 2%;
+      left: 2%;
       padding: 20px;
       box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+      width: 80%;
+      height: 80%;
+    }
+    #cartButton {
+      position: absolute;
+      top: 0vw;
+      right: 0vw;
+      width: 40px;
+      height: 40px;
+      background-color: #f0f0f0;
+      border-radius: 50%;
+      cursor: pointer;
     }
   </style>
 `;
