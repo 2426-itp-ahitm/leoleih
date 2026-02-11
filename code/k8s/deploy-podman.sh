b012235a2b8334ece4a98da265e0a358dd90c038
Timon Rosenbichler <t.rosenbichler@students.htl-leonding.ac.at>
2026-02-11 09:23:10 +0100

    added thi

--- code/k8s/deploy-podman.sh
+++ code/k8s/deploy-podman.sh
@@ -38,9 +38,9 @@ cd ../frontend/docker
 #docker build --no-cache . -t ghcr.io/s-stoeger/shop-frontend:latest
 #docker push ghcr.io/s-stoeger/shop-frontend:latest
 
-docker build --no-cache . -t ghcr.io/s-stoeger/shop-frontend:$TAG -t ghcr.io/s-stoeger/shop-frontend:latest
-docker push ghcr.io/s-stoeger/shop-frontend:$TAG
-docker push ghcr.io/s-stoeger/shop-frontend:latest
+podman build --no-cache . -t ghcr.io/s-stoeger/shop-frontend:$TAG -t ghcr.io/s-stoeger/shop-frontend:latest
+podman push ghcr.io/s-stoeger/shop-frontend:$TAG
+podman push ghcr.io/s-stoeger/shop-frontend:latest
 
 echo "----------------------------------"
 echo SUCCESSFULLY BUILT AND PUSHED FRONTEND
@@ -51,9 +51,9 @@ cd ../../backend/omnial/src/main/docker
 
 #docker build --no-cache . -t ghcr.io/s-stoeger/backend:latest
 #docker push ghcr.io/s-stoeger/backend:latest
-docker build --no-cache . -t ghcr.io/s-stoeger/backend:$TAG -t ghcr.io/s-stoeger/backend:latest
-docker push ghcr.io/s-stoeger/backend:$TAG
-docker push ghcr.io/s-stoeger/backend:latest
+podman build --no-cache . -t ghcr.io/s-stoeger/backend:$TAG -t ghcr.io/s-stoeger/backend:latest
+podman push ghcr.io/s-stoeger/backend:$TAG
+podman push ghcr.io/s-stoeger/backend:latest
 
 echo "----------------------------------"
 echo SUCCESSFULLY BUILT AND PUSHED BACKEND
@@ -65,6 +65,7 @@ cd ../../../../..
 kubectl delete configmap nginx-config || echo "nginx-config does not yet exist"
 kubectl create configmap nginx-config --from-file ./frontend/docker/default.conf
 
-kubectl rollout restart deployment nginx
-kubectl rollout restart deployment appsrv
+kubectl rollout restart deployment/appsrv -n "$NAMESPACE"
+kubectl rollout restart deployment/nginx -n "$NAMESPACE"
+kubectl rollout restart deployment/postgres -n "$NAMESPACE"
 kubectl get pods
