9beeb347c41f7f55e812ee4cc059031fe522c0e1
ChristophKorzeniowski <c.korzeniowski@students.htl-leonding.ac.at>
2026-04-06 01:38:58 +0200

    Geil(geil)

--- code/frontend/src/app/user.service.ts
+++ code/frontend/src/app/user.service.ts
@@ -9,7 +9,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class UserService implements OnDestroy {
   private http: HttpClient = inject(HttpClient);
   //private URL: string = 'https://it200281.cloud.htl-leonding.ac.at/api/';
-  private URL: string = 'https://it210195.cloud.htl-leonding.ac.at/api/';
+  //private URL: string = 'https://it210195.cloud.htl-leonding.ac.at/api/';
+  private URL: string = 'http://localhost:8080/api/';
 
 
   // BehaviorSubject to track current user
